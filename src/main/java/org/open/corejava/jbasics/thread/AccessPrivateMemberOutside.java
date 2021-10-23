8406c67c55c3620ad2c1dfb78e60b0061c9c885c
Mahadev Mane <mahadevmane@gmail.com>
2021-10-23 13:34:11 +0530

    Fixed build errors and warnings

--- src/main/java/org/open/corejava/jbasics/thread/AccessPrivateMemberOutside.java
+++ src/main/java/org/open/corejava/jbasics/thread/AccessPrivateMemberOutside.java
@@ -1,15 +1,7 @@
 package org.open.corejava.jbasics.thread;
 
-import java.lang.reflect.Field;
-
 public class AccessPrivateMemberOutside {
-
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
-            IllegalAccessException, SecurityException, NoSuchFieldException {
-        Class<?> c = Class.forName("HoneyMoon");
-        Object o = c.newInstance();
-        Field f = c.getDeclaredField("name");
-        f.setAccessible(true);
-        System.out.println(f.get(o));
+    public static void main(String[] args) {
+        //TODO: Put implementation here for accessing private member outside class
     }
 }
